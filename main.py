3f53275ae7ad3a314b616a075545390d95cf66b3
Ilya K <131490186+umphmeow@users.noreply.github.com>
2023-05-16 13:38:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import timeit
 
-# Определяем границы применимости рекурсивного и итерационного подходов
-max_recursion_depth = 3000 # Максимальная глубина рекурсии
-max_memory_usage = 1000000 # Максимальный объем памяти (в байтах)
+max_recursion_depth = 3000 
 
-# Функция для вычисления значения функции F(n) рекурсивно с memo
+
 memo_F = {1: 1}
 def F_recu(n):
     if n in memo_F:
@@ -16,7 +14,7 @@ def F_recu(n):
         memo_F[n] = math.sin(F_recu(n-1)) - G_recu(n-1)
         return memo_F[n]
 
-# Функция для вычисления значения функции G(n) рекурсивно с memo
+
 memo_G = {1: 1}
 memo_factorial = {1: 1}
 def G_recu(n):
@@ -32,14 +30,12 @@ def G_recu(n):
         memo_G[n] = 2*F_n_1 - factorial_n_1
         return memo_G[n]
 
-# Функция для вычисления значения функции F(n) итерационно
+
 def F_iter(n):
-    # Инициализируем массивы F и G
     F = [0] * (n+1)
     G = [0] * (n+1)
     F[1] = G[1] = 1
 
-    # Вычисляем значения F и G для всех n от 2 до n
     for i in range(2, n+1):
         F[i] = math.sin(F[i-1]) - G[i-1]
         G[i] = 2*F[i-1] - math.factorial(i-1)
@@ -54,7 +50,6 @@ n_values = [int(n) for n in n_values if n.isdigit() and 1 <= int(n) <= 171]
 if len(n_values) == 0:
     print("Вы не ввели корректные значения n.")
 else:
-    # Вычисляем значения функций F(n) и G(n) с помощью timeit
     recursive_times = []
     iterative_times = []
     for n in n_values:
@@ -63,7 +58,6 @@ else:
         iterative_time = timeit.timeit(lambda: F_iter(n), number=100)
         iterative_times.append(iterative_time)
 
-    # Сохраняем результаты вычислений в таблицу
     data = {'n': n_values, 'recursive_time': recursive_times, 'iterative_time': iterative_times}
     df = pd.DataFrame(data)
 
