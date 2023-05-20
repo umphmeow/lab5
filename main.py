bbc0e91c83bf9a787529f2b17dd3dbe07e03d827
Ilya K <131490186+umphmeow@users.noreply.github.com>
2023-05-20 12:44:11 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,48 +3,66 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import timeit
 
-memory_F = {1: 1}
+max_recursion_depth = 3000
+
+
+memo_F = {1: 1}
 def F_recu(n):
-    if n in memory_F:
-        return memory_F[n]
+    if n in memo_F:
+        return memo_F[n]
     else:
-        memory_F[n] = math.sin(F_recu(n - 1)) - G_recu(n - 1)
-        return memory_F[n]
+        memo_F[n] = math.sin(F_recu(n-1)) - G_recu(n-1)
+        return memo_F[n]
 
 
-memory_G = {1: 1}
-memory_factorial = {1: 1}
+memo_G = {1: 1}
+memo_factorial = {1: 1}
 def G_recu(n):
-    if n in memory_G:
-        return memory_G[n]
+    if n in memo_G:
+        return memo_G[n]
     else:
         F_n_1 = F_recu(n - 1)
-        if n-1 in memory_factorial:
-            factorial_n_1 = memory_factorial[n - 1]
+        if n-1 in memo_factorial:
+            factorial_n_1 = memo_factorial[n-1]
         else:
             factorial_n_1 = math.factorial(n - 1)
-            memory_factorial[n - 1] = factorial_n_1
-        memory_G[n] = 2 * F_n_1 - factorial_n_1
-        return memory_G[n]
+            memo_factorial[n-1] = factorial_n_1
+        memo_G[n] = 2*F_n_1 - factorial_n_1
+        return memo_G[n]
 
+
+memo_F_iter = {1: 1}
+memo_G_iter = {1: 1}
+memo_factorial_iter = {1: 1}
 
 def F_iter(n):
-    F_1 = F_2 = 1
-    G_1 = G_2 = 1
+    if n in memo_F_iter:
+        return memo_F_iter[n]
+    else:
+        F_n = F_iter(n - 1) if n - 1 in memo_F_iter else math.sin(F_iter(n - 1)) - G_iter(n - 1)
+        memo_F_iter[n] = F_n
+        G_n = G_iter(n - 1) if n - 1 in memo_G_iter else 2 * F_iter(n - 1) - math.factorial(n - 1)
+        memo_G_iter[n] = G_n
+        return F_n
 
-    for i in range(2, n+1):
-        F = math.sin(F_1) - G_1
-        G = 2*F_1 - math.factorial(i-1)
-
-        F_2, F_1 = F_1, F
-        G_2, G_1 = G_1, G
-
-    return F
+def G_iter(n):
+    if n in memo_G_iter:
+        return memo_G_iter[n]
+    else:
+        F_n_1 = F_iter(n - 1) if n-1 in memo_F_iter else math.sin(F_iter(n-1)) - G_iter(n-1)
+        memo_F_iter[n-1] = F_n_1
+        if n-1 in memo_factorial_iter:
+            factorial_n_1 = memo_factorial_iter[n-1]
+        else:
+            factorial_n_1 = math.factorial(n - 1)
+            memo_factorial_iter[n-1] = factorial_n_1
+        memo_G_iter[n] = 2*F_n_1 - factorial_n_1
+        return memo_G_iter[n]
 
 print("Программа вычисляет значение функции F для каждого введеного n двумя способами рекурсивно и итерационо: F(n) = sin(F(n–1)) – G(n–1),где G(n) = 2*F(n–1) - (n–1)!")
 
-n_values = input("Введите значения n через пробел, не превышающие 171, но больше 0: ").split()
-n_values = [int(n) for n in n_values if n.isdigit() and 1 <= int(n) <= 173]
+n_values = input("Введите значения n через пробел, не превышающие 171: ").split()
+n_values = [int(n) for n in n_values if n.isdigit() and 1 <= int(n) <= 200]
 
 if len(n_values) == 0:
     print("Вы не ввели корректные значения n.")
@@ -64,8 +82,8 @@ else:
     print(df)
 
     # Строим график зависимости времени выполнения от значения n
-    plt.plot(n_values, recursive_times, 'ro', label='Recursive')
-    plt.plot(n_values, iterative_times, 'bo', label='Iterative')
+    plt.plot(n_values, recursive_times, label='Recursive')
+    plt.plot(n_values, iterative_times, label='Iterative')
     plt.xlabel('n')
     plt.ylabel('Time (seconds)')
     plt.title('Time for calculating F(n)')
